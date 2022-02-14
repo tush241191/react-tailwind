8e89ef429350dd0f3629e586ee9707cd8b8effe4
Tushar Kharat <tush241191@gmail.com>
2022-02-15 01:12:55 +0530

    [add] Select location modal

--- src/views/plays/Details.js
+++ src/views/plays/Details.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "../../components/Navbars/Navbar";
 
 import slide1 from "../../assets/img/5.jpeg";
@@ -6,6 +7,7 @@ import play2 from "../../assets/img/p2.jpeg";
 import play3 from "../../assets/img/p3.jpeg";
 import play4 from "../../assets/img/p4.jpeg";
 import GridView from "../../components/Cards/GridView";
+
 
 const PlayDetails = () => {
   const files = [
@@ -65,15 +67,19 @@ const PlayDetails = () => {
     },
   ];
 
+  const [open, setOpen] = useState(false)
+
+  const [bookNowSlide, setBookNowSlide] = useState(1);
+
   return (
     <>
       <Navbar />
       <main>
-        <div className="bg-gray-100">
+        <div className="bg-gray-100 pb-10">
           <div>
             <div className="bg-gray-900 relative">
               <div
-                class="absolute inset-0 bg-gray-900 bg-opacity-50 transition-opacity"
+                className="absolute inset-0 bg-gray-900 bg-opacity-50 transition-opacity"
                 aria-hidden="true"
               ></div>
               <img
@@ -84,6 +90,7 @@ const PlayDetails = () => {
               <div className="w-full h-40 bg-gray-900"></div>
             </div>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 relative">
+              {/* Header slide */}
               <div className="-mt-12 sm:-mt-80 sm:flex sm:items-start sm:space-x-5">
                 <div className="w-full flex items-end">
                   <img
@@ -103,7 +110,7 @@ const PlayDetails = () => {
                       <h3 className="text-sm text-gray-300 flex items-center">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          class="h-4 w-4 mr-1"
+                          className="h-4 w-4 mr-1"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
@@ -120,6 +127,7 @@ const PlayDetails = () => {
                     </div>
                     <div className="">
                       <button
+                        onClick={()=> setOpen(true)}
                         type="button"
                         className="inline-flex items-center px-6 py-3  text-base font-medium rounded-full shadow-sm text-gray-100 bg-gradient-to-r from-red-600 to-blue-700 hover:bg-gradient-to-l hover:from-red-600 hover:to-blue-700 focus:outline-none"
                       >
@@ -129,8 +137,11 @@ const PlayDetails = () => {
                   </div>
                 </div>
               </div>
+              {/* Header slide end */}
+              {/* Show detail start */}
               <div className="mt-28 min-w-0 flex-1">
                 <div className="grid grid-cols-4 gap-4">
+                  {/* Show crew start */}
                   <div className="space-y-6">
                     <div className="bg-white p-4 rounded-md">
                       <h2 className="text-xl text-gray-900 font-semibold">
@@ -196,6 +207,8 @@ const PlayDetails = () => {
                       </div>
                     </div>
                   </div>
+                  {/* Show crew end */}
+                  {/* Show summary start */}
                   <div className="col-span-2 bg-white p-4 rounded-md">
                     <div>
                       <h2 className="text-xl text-gray-900 font-semibold">
@@ -230,10 +243,10 @@ const PlayDetails = () => {
                     </div>
                     <div className="mt-8">
                       <div
-                        class="inset-0 flex items-center pb-4"
+                        className="inset-0 flex items-center pb-4"
                         aria-hidden="true"
                       >
-                        <div class="w-full border-t border-gray-300"></div>
+                        <div className="w-full border-t border-gray-300"></div>
                       </div>
                       <h2 className="text-xl text-gray-900 font-semibold">
                         Summary
@@ -253,10 +266,10 @@ const PlayDetails = () => {
                     </div>
                     <div className="mt-8">
                       <div
-                        class="inset-0 flex items-center pb-4"
+                        className="inset-0 flex items-center pb-4"
                         aria-hidden="true"
                       >
-                        <div class="w-full border-t border-gray-300"></div>
+                        <div className="w-full border-t border-gray-300"></div>
                       </div>
                       <h2 className="text-xl text-gray-900 font-semibold">
                         Related
@@ -266,30 +279,249 @@ const PlayDetails = () => {
                       </div>
                     </div>
                   </div>
+                  {/* Show summary end */}
+                  {/* Show location start */}
                   <div className="bg-white p-4 rounded-md">
                     <h2 className="text-xl text-gray-900 font-semibold">
                       Location
                     </h2>
                     <div className="divide-y">
                       <div className="py-4">
-                        <h3 className="text-base text-gray-700">Vishnudas Bhave</h3>
-                        <p className="mt-2 text-sm text-gray-400">Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk, Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी बस डिपो, Juhu Nagar, Sector-16, Vashi, Navi Mumbai, Maharashtra 400703</p>
+                        <h3 className="text-base text-gray-700">
+                          Vishnudas Bhave
+                        </h3>
+                        <p className="mt-2 text-xs text-gray-400">
+                          Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk,
+                          Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी बस
+                          डिपो, Juhu Nagar, Sector-16, Vashi, Navi Mumbai,
+                          Maharashtra 400703
+                        </p>
                       </div>
                       <div className="py-4">
-                        <h3 className="text-base text-gray-700">Vishnudas Bhave</h3>
-                        <p className="mt-2 text-sm text-gray-400">Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk, Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी बस डिपो, Juhu Nagar, Sector-16, Vashi, Navi Mumbai, Maharashtra 400703</p>
+                        <h3 className="text-base text-gray-700">
+                          Vishnudas Bhave
+                        </h3>
+                        <p className="mt-2 text-xs text-gray-400">
+                          Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk,
+                          Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी बस
+                          डिपो, Juhu Nagar, Sector-16, Vashi, Navi Mumbai,
+                          Maharashtra 400703
+                        </p>
                       </div>
                       <div className="py-4">
-                        <h3 className="text-base text-gray-700">Vishnudas Bhave</h3>
-                        <p className="mt-2 text-sm text-gray-400">Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk, Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी बस डिपो, Juhu Nagar, Sector-16, Vashi, Navi Mumbai, Maharashtra 400703</p>
+                        <h3 className="text-base text-gray-700">
+                          Vishnudas Bhave
+                        </h3>
+                        <p className="mt-2 text-xs text-gray-400">
+                          Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk,
+                          Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी बस
+                          डिपो, Juhu Nagar, Sector-16, Vashi, Navi Mumbai,
+                          Maharashtra 400703
+                        </p>
                       </div>
                     </div>
                   </div>
+                  {/* Show location end */}
                 </div>
               </div>
             </div>
           </div>
         </div>
+        {/* Modal start */}
+        <div
+          className={`${open ? "" : "hidden"} fixed z-10 inset-0 overflow-y-auto`}
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 bg-gray-900 bg-opacity-80 transition-opacity"
+              aria-hidden="true"
+            ></div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl shadow-blue-600/50 transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full">
+              <div className="bg-white pt-5 pb-4 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                    <div className="flex items-center justify-between">
+                      <h3
+                        className="text-xl ml-4 leading-6 font-medium text-gray-900"
+                        id="modal-title"
+                      >
+                        Eka Lagnachi Pudhchi Gosht
+                      </h3>
+                      <svg
+                        onClick={()=> setOpen(false)}
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="text-red-400 hover:text-red-700 h-6 w-6 mr-4 font-thin cursor-pointer"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </div>
+                    <div className="mt-2">
+                      <div className="divide-y">
+                        <div className="flex items-center justify-between p-4 hover:bg-gray-100 cursor-pointer">
+                          <div>
+                            <h3 className="text-base text-gray-700 font-semibold">
+                              Vishnudas Bhave
+                            </h3>
+                            <span className="text-sm text-gray-700">
+                              18 feb 2022 4:00 PM
+                            </span>
+                            <p className="mt-2 text-xs text-gray-400">
+                              Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk,
+                              Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी
+                              बस डिपो, Juhu Nagar, Sector-16, Vashi, Navi
+                              Mumbai, Maharashtra 400703
+                            </p>
+                          </div>
+                          <div>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-4 w-4 font-thin"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 5l7 7-7 7"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between p-4 hover:bg-gray-100 cursor-pointer">
+                          <div>
+                            <h3 className="text-base text-gray-700 font-semibold">
+                              Vishnudas Bhave
+                            </h3>
+                            <span className="text-sm text-gray-700">
+                              18 feb 2022 4:00 PM
+                            </span>
+                            <p className="mt-2 text-xs text-gray-400">
+                              Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk,
+                              Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी
+                              बस डिपो, Juhu Nagar, Sector-16, Vashi, Navi
+                              Mumbai, Maharashtra 400703
+                            </p>
+                          </div>
+                          <div>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-4 w-4 font-thin"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 5l7 7-7 7"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between p-4 hover:bg-gray-100 cursor-pointer">
+                          <div>
+                            <h3 className="text-base text-gray-700 font-semibold">
+                              Vishnudas Bhave
+                            </h3>
+                            <span className="text-sm text-gray-700">
+                              18 feb 2022 4:00 PM
+                            </span>
+                            <p className="mt-2 text-xs text-gray-400">
+                              Juhu-Vashi Road,Chhatrapati Shivaji Maharaj Chowk,
+                              Sector-16A, Vashi Kopar Khairane Rd, Opposite वाशी
+                              बस डिपो, Juhu Nagar, Sector-16, Vashi, Navi
+                              Mumbai, Maharashtra 400703
+                            </p>
+                          </div>
+                          <div>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-4 w-4 font-thin"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 5l7 7-7 7"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-900 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full flex items-center justify-center rounded-md border border-transparent shadow-sm px-4 py-1.5 bg-orange-600 text-base font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Next
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="ml-1 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M11.933 12.8a1 1 0 000-1.6L6.6 7.2A1 1 0 005 8v8a1 1 0 001.6.8l5.333-4zM19.933 12.8a1 1 0 000-1.6l-5.333-4A1 1 0 0013 8v8a1 1 0 001.6.8l5.333-4z"
+                    />
+                  </svg>
+                </button>
+                <button
+                  type="button"
+                  className={`${bookNowSlide == 1 ? "bg-gray-300 cursor-not-allowed" : "bg-white hover:bg-gray-50 cursor-pointer"} mt-3 w-full flex items-center justify-center rounded-md border border-gray-300 shadow-sm px-4 py-1.5 text-base font-medium text-gray-700 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm`}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="mr-1 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z"
+                    />
+                  </svg>
+                  Previous
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Modal end */}
       </main>
     </>
   );
