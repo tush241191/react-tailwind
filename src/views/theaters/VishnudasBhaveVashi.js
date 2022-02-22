93ce62e2fe1d23f7733630095243c21c4de30b57
Tushar Kharat <tush241191@gmail.com>
2022-02-22 23:00:56 +0530

    [add]vashi theater layout

--- src/views/theaters/VishnudasBhaveVashi.js
+++ src/views/theaters/VishnudasBhaveVashi.js
@@ -1,10 +1,479 @@
-
 const VishnudasBhaveVashi = () => {
+  const rows = [
+    {
+      name: "A",
+      size: 28,
+      space: [
+        {
+          name: 7,
+          count: 2,
+        },
+        {
+          name: 14,
+          count: 2,
+        },
+        {
+          name: 21,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "B",
+      size: 32,
+      space: [
+        {
+          name: 8,
+          count: 2,
+        },
+        {
+          name: 16,
+          count: 2,
+        },
+        {
+          name: 24,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "C",
+      size: 32,
+      space: [
+        {
+          name: 8,
+          count: 2,
+        },
+        {
+          name: 16,
+          count: 2,
+        },
+        {
+          name: 24,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "D",
+      size: 33,
+      space: [
+        {
+          name: 8,
+          count: 2,
+        },
+        {
+          name: 17,
+          count: 2,
+        },
+        {
+          name: 25,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "E",
+      size: 36,
+      space: [
+        {
+          name: 9,
+          count: 2,
+        },
+        {
+          name: 18,
+          count: 2,
+        },
+        {
+          name: 27,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "F",
+      size: 36,
+      space: [
+        {
+          name: 9,
+          count: 2,
+        },
+        {
+          name: 18,
+          count: 2,
+        },
+        {
+          name: 27,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "G",
+      size: 39,
+      space: [
+        {
+          name: 10,
+          count: 2,
+        },
+        {
+          name: 20,
+          count: 2,
+        },
+        {
+          name: 29,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "H",
+      size: 40,
+      space: [
+        {
+          name: 10,
+          count: 2,
+        },
+        {
+          name: 20,
+          count: 2,
+        },
+        {
+          name: 30,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "I",
+      size: 42,
+      space: [
+        {
+          name: 11,
+          count: 2,
+        },
+        {
+          name: 21,
+          count: 2,
+        },
+        {
+          name: 31,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "J",
+      size: 42,
+      space: [
+        {
+          name: 11,
+          count: 2,
+        },
+        {
+          name: 21,
+          count: 2,
+        },
+        {
+          name: 31,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "K",
+      size: 43,
+      space: [
+        {
+          name: 11,
+          count: 2,
+        },
+        {
+          name: 22,
+          count: 2,
+        },
+        {
+          name: 32,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "empty",
+    },
+    {
+      name: "L",
+      size: 46,
+      space: [
+        {
+          name: 12,
+          count: 2,
+        },
+        {
+          name: 23,
+          count: 2,
+        },
+        {
+          name: 34,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "M",
+      size: 46,
+      space: [
+        {
+          name: 12,
+          count: 2,
+        },
+        {
+          name: 23,
+          count: 2,
+        },
+        {
+          name: 34,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "N",
+      size: 47,
+      space: [
+        {
+          name: 12,
+          count: 2,
+        },
+        {
+          name: 24,
+          count: 2,
+        },
+        {
+          name: 35,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "O",
+      size: 50,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 25,
+          count: 2,
+        },
+        {
+          name: 37,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "P",
+      size: 50,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 25,
+          count: 2,
+        },
+        {
+          name: 37,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "Q",
+      size: 51,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 26,
+          count: 2,
+        },
+        {
+          name: 38,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "R",
+      size: 50,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 25,
+          count: 2,
+        },
+        {
+          name: 37,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "balcony",
+    },
+    {
+      name: "S",
+      size: 50,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 25,
+          count: 2,
+        },
+        {
+          name: 37,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "T",
+      size: 50,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 25,
+          count: 2,
+        },
+        {
+          name: 37,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "U",
+      size: 52,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 26,
+          count: 2,
+        },
+        {
+          name: 39,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "V",
+      size: 54,
+      space: [
+        {
+          name: 14,
+          count: 2,
+        },
+        {
+          name: 27,
+          count: 2,
+        },
+        {
+          name: 40,
+          count: 2,
+        },
+      ],
+    },
+    {
+      name: "W",
+      size: 52,
+      space: [
+        {
+          name: 13,
+          count: 2,
+        },
+        {
+          name: 26,
+          count: 2,
+        },
+        {
+          name: 39,
+          count: 2,
+        },
+      ],
+    },
+  ];
+
+  function checkSpace(index, obj) {
+    let space = obj.find((key) => key.name == index);
+    if (space != undefined) {
+      return [...Array(space.count)].map((e, i) => (
+        <td className="border border-transparent w-6 h-6">&nbsp;</td>
+      ));
+    }
+  }
   return (
     <>
-      <h1>VishnudasBhaveVashi</h1>
+      <div className="w-fit mx-auto overflow-x-auto px-10">
+        <table className="w-full my-10 flex flex-col-reverse items-center">
+          {rows.map((row, index) => (
+            <>
+              {row.name == "empty" ? (
+                <>
+                  <tr>
+                    <td>&nbsp;</td>
+                  </tr>
+                </>
+              ) : row.name == "balcony" ? (
+                <>
+                  <tr>
+                    <td>&nbsp;</td>
+                  </tr>
+                  <tr className="w-full">
+                    <td className="w-full flex items-center justify-center text-sm font-semibold uppercase">Balcony</td>
+                  </tr>
+                  <tr>
+                    <td>&nbsp;</td>
+                  </tr>
+                </>
+              ) : (
+                <tr className="flex items-center flex-row-reverse py-1">
+                  <td className="text-sm mx-1 font-semibold">{row.name}</td>
+                  {[...Array(row.size)].map((e, i) => (
+                    <>
+                      {checkSpace(i, row.space)}
+                      <td className="border w-6 h-6 text-xs text-gray-500 flex items-center justify-center">
+                        {i + 1}
+                      </td>
+                    </>
+                  ))}
+                  <td className="text-sm mx-1 font-semibold">{row.name}</td>
+                </tr>
+              )}
+            </>
+          ))}
+        </table>
+      </div>
     </>
   );
 };
 
-export default VishnudasBhaveVashi
+export default VishnudasBhaveVashi;
