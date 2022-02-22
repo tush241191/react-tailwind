25094459e18186c4089a3eea364c37a2e9e52013
Tushar Kharat <tush241191@gmail.com>
2022-02-23 00:16:28 +0530

    responsive theater layout

--- src/views/theaters/VishnudasBhaveVashi.js
+++ src/views/theaters/VishnudasBhaveVashi.js
@@ -432,45 +432,58 @@ const VishnudasBhaveVashi = () => {
   }
   return (
     <>
-      <div className="w-fit mx-auto overflow-x-auto px-10">
-        <table className="w-full my-10 flex flex-col-reverse items-center">
-          {rows.map((row, index) => (
-            <>
-              {row.name == "empty" ? (
-                <>
-                  <tr>
-                    <td>&nbsp;</td>
-                  </tr>
-                </>
-              ) : row.name == "balcony" ? (
-                <>
-                  <tr>
-                    <td>&nbsp;</td>
-                  </tr>
-                  <tr className="w-full">
-                    <td className="w-full flex items-center justify-center text-sm font-semibold uppercase">Balcony</td>
-                  </tr>
-                  <tr>
-                    <td>&nbsp;</td>
-                  </tr>
-                </>
-              ) : (
-                <tr className="flex items-center flex-row-reverse py-1">
-                  <td className="text-sm mx-1 font-semibold">{row.name}</td>
-                  {[...Array(row.size)].map((e, i) => (
-                    <>
-                      {checkSpace(i, row.space)}
-                      <td className="border w-6 h-6 text-xs text-gray-500 flex items-center justify-center">
-                        {i + 1}
-                      </td>
-                    </>
-                  ))}
-                  <td className="text-sm mx-1 font-semibold">{row.name}</td>
-                </tr>
-              )}
-            </>
-          ))}
-        </table>
+      <div className="max-w-screen-2xl mx-auto flex flex-col overflow-hidden">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full w-full my-10 flex flex-col-reverse items-center overflow-auto px-4">
+                {rows.map((row, index) => (
+                  <>
+                    {row.name == "empty" ? (
+                      <>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                      </>
+                    ) : row.name == "balcony" ? (
+                      <>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                        <tr className="w-full">
+                          <td className="w-full flex items-center justify-center text-sm font-semibold uppercase">
+                            Balcony
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>&nbsp;</td>
+                        </tr>
+                      </>
+                    ) : (
+                      <tr className="flex items-center flex-row-reverse py-1">
+                        <td className="text-sm mx-1 font-semibold">
+                          {row.name}
+                        </td>
+                        {[...Array(row.size)].map((e, i) => (
+                          <>
+                            {checkSpace(i, row.space)}
+                            <td className="border w-6 h-6 text-xs text-gray-500 flex items-center justify-center">
+                              {i + 1}
+                            </td>
+                          </>
+                        ))}
+                        <td className="text-sm mx-1 font-semibold">
+                          {row.name}
+                        </td>
+                      </tr>
+                    )}
+                  </>
+                ))}
+              </table>
+              <div className="h-10 w-full max-w-md mx-auto text-sm font-medium border flex items-center justify-center mb-10">STAGE</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
